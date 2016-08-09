84e0df8e0d10789ede8fd608de6c7198e67fbcdc
Raphael Barbo <rsbarbo@gmail.com>
2016-08-09 10:59:03 -0600

    added task_manager to github

---
+++ app/models/task.rb
@@ -0,0 +1,12 @@
+class Task
+  attr_reader :title,
+              :description,
+              :id
+
+  def initialize(data)
+    @id = data["id"]
+    @title = data["title"]
+    @description = data["description"]
+  end
+
+end
