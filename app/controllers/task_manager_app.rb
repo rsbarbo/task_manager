6870cd4bba69a79e6e04f164e8067cd028326ac0
Raphael Barbo <rsbarbo@gmail.com>
2016-08-16 15:26:20 -0600

    some more changes in class, added delete option, and update

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,8 @@
-  require 'models/task_manager'
+require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -25,9 +26,24 @@ class TaskManagerApp < Sinatra::Base
     redirect '/tasks'
   end
 
-  get '/tasks/:id' do |id|
-    @task = task_manager.find(id.to_i)
+  get '/tasks/:id' do
+    @task = task_manager.find(params[:id].to_i)
     erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = task_manager.find(params[:id].to_i)
+    erb :edit
+  end
+
+  put '/tasks/:id' do
+    task_manager.update(params[:id].to_i, params[:task])
+    redirect "/tasks/#{params[:id]}"
+  end
+
+  delete '/tasks/:id' do
+    task_manager.destroy(params[:id].to_i)
+    redirect "/tasks"
   end
 
   def task_manager
